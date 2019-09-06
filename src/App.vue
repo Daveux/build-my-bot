3fb8550d10df2ac63cbccadd653ffa9220109797
David Odeyinka <david.odeyinka@yahoo.com>
2019-09-06 12:45:45 +0100

    updates parts with v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <img src="./assets/build-a-bot-logo.png" class="logo">
+            Build-a-Bot!
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-     <HomePage/>
+     <RobotBuilder/>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
-
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 export default {
   name: 'app',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
@@ -33,5 +43,25 @@ export default {
     width: 1024px;
     min-height: 300px;
 
+  }
+  header {
+    background-color: #999;
+    width: 1084px;
+    margin: 0 auto;
+  }
+  ul {
+    padding: 3px;
+    display: flex;
+    /*text-decoration: none;*/
+  }
+  .nav-item {
+    display: inline-block;
+    padding: 5px 10px;
+    font-size: 22px;
+    border-right: 1px solid #bbb;
+  }
+  .logo {
+    vertical-align: middle;
+    height: 30px;
   }
 </style>
